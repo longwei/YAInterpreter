23234f12ba0d3c786fc4b945d87b05eb8036b351
Longwei Su <longwei.su@gmail.com>
2014-09-30 00:17:46 -0400

    finished up with variables , function, invoke

--- README.md
+++ README.md
@@ -1,10 +1,65 @@
 How to make a interpreter?
+===
+
+It accept a expression and return a value.
 
 * parsing such as lex or yacc are just for output AST, so as long as the formal programe structure is like scheme, then we could save time by directly going to semantics level
 
-* match, sort of like regex's pattern match, find the match patten then bind vars to symbols
+* match, sort of like regex's pattern match, find the match patten then bind vars to symbols. (,op ,e1 ,e2) is a pattern to match the same structure like (+ 1 2).
+
+* CBV: call-by-value; CBN: call-by-need(or Lazy evaluation)
+
+
+Lamda calculus
+===
 
 * lambda calcuclus only have three primitive
-  * var
+  * var -- binding & evaluatie
   * function
-  * invokcation 
+  * invokcation
+  
+* multi parameter
+
+```
+  ----------
+  |    --- |
+  | -y | | |
+-x|    --- |
+  ----------
+  ((
+  (lambda (x) (lambda (y) (* y y)))
+  2) 3)
+  
+  ((f 2)3)
+```
+
+* scope is for record the value
+
+```
+(define lookup
+(lambda (x env)
+    (let ([p (assq x env)])
+      (cond
+       [(not p) x]
+       [else (cdr p)]))))
+     
+((x.1)(y.2)(z.3))
+     
+(define ext-env
+  (lambda (x v env)
+    (cons `(,x . ,v) env)))
+    
+insert a new part to front of the list
+```
+* closure: 
+```(lambda (y) (lambda (x) (* y 2)))```, it can't just return (* y 2) as a function, because y is in the inner scope. That's why we need to have a closure to remeber outer function's parameter. (a.k.a lexical scoping or static scoping)
+
+dynamic scoping is just returning the function body itself, and it is evaluate it value on invokecation. which could be both useful and buggie
+
+```
+(struct Closure (f env))
+
+ [`(lambda (,x) ,e)
+   (Closure exp env)]
+   
+```
--- calc.rkt
+++ calc.rkt
@@ -1,13 +1,72 @@
 #lang racket
-(define calc
-  (lambda (exp)
-    (match exp                                
-      [(? number? x) x]                       
-      [`(,op ,e1 ,e2)                         
-       (let ([v1 (calc e1)]                   
-             [v2 (calc e2)])                  
-         (match op                            
-           ['+ (+ v1 v2)]                     
-           ['- (- v1 v2)]                     
+
+(define env0 '())
+
+;; extend the x at front of list
+(define ext-env
+  (lambda (x v env)
+    (cons `(,x . ,v) env)))
+
+;; lookup x in association list
+(define lookup
+  (lambda (x env)
+    (let ([p (assq x env)])
+      (cond
+       [(not p) x]
+       [else (cdr p)]))))
+
+(struct Closure (f env))
+
+;; input: exp and env
+;; 5 condition: symbol, number, function, invoke, expression
+(define interp1
+  (lambda (exp env)
+    (match exp                                          
+      [(? symbol? x) (lookup x env)]                    
+      [(? number? x) x]                                 
+      [`(lambda (,x) ,e)                                ; function
+       (Closure exp env)]
+      [`(,e1 ,e2)                                       ; invoke
+       (let ([v1 (interp1 e1 env)]
+             [v2 (interp1 e2 env)])
+         (match v1
+           [(Closure `(lambda (,x) ,e) env1)            ; using env1 as we want to evulate it by the env it invoked
+            (interp1 e (ext-env x v2 env1))]))]         ; if current env instead of env1, then dynamic scoping
+      [`(,op ,e1 ,e2)                                   
+       (let ([v1 (interp1 e1 env)]
+             [v2 (interp1 e2 env)])
+         (match op
+           ['+ (+ v1 v2)]
+           ['- (- v1 v2)]
            ['* (* v1 v2)]
            ['/ (/ v1 v2)]))])))
+
+;;wrapper for additional parameters
+(define interp
+  (lambda (exp)
+    (interp1 exp env0)))
+
+
+(interp '(+ 1 2))
+;; => 3
+
+(interp '(* 2 3))
+;; => 6
+
+(interp '(* 2 (+ 3 4)))
+;; => 14
+
+(interp '(* (+ 1 2) (+ 3 4)))
+;; => 21
+
+(interp '(((lambda (x) (lambda (y) (* x y))) 2) 3))
+;; => 6
+
+(interp '((lambda (x) (* 2 x)) 3))
+;; => 6
+
+(interp '((lambda (y) (((lambda (y) (lambda (x) (* y 2))) 3) 0)) 4))
+;; => 6
+
+;; (interp '(1 2))
+;; => match: no matching clause for 1
